5ace6a788a9623e010d8508c6d1b38cf5b1f854c
Léo Rolland <rolland.leo@orange.fr>
2025-10-31 16:15:34 +0100

    add random collector

---
+++ pkg/infrastructure/collector/random.go
@@ -0,0 +1,41 @@
+package collector
+
+import (
+	"math/rand/v2"
+	"time"
+
+	"github.com/leorolland/sortir.in/pkg/application"
+)
+
+type randomCollector struct {
+}
+
+func NewRandomCollector() application.Collector {
+	return &randomCollector{}
+}
+
+func (c *randomCollector) Collect(location application.CollectLocation) ([]application.Event, error) {
+	return []application.Event{
+		{
+			Name: "Random Event 1",
+			Lat:  location.Lat + rand.Float64() * 0.1,
+			Lon:  location.Long + rand.Float64() * 0.1,
+			Begin: time.Now().Add(time.Hour * 24),
+			End:   time.Now().Add(time.Hour * 24 * 2),
+		},
+		{
+			Name: "Random Event 2",
+			Lat:  location.Lat + rand.Float64() * 0.1,
+			Lon:  location.Long + rand.Float64() * 0.1,
+			Begin: time.Now().Add(time.Hour * 24 * 2),
+			End:   time.Now().Add(time.Hour * 24 * 3),
+		},
+		{
+			Name: "Random Event 3",
+			Lat:  location.Lat + rand.Float64() * 0.1,
+			Lon:  location.Long + rand.Float64() * 0.1,
+			Begin: time.Now().Add(time.Hour * 24 * 3),
+			End:   time.Now().Add(time.Hour * 24 * 4),
+		},
+	}, nil
+}
---
+++ pkg/infrastructure/collector/random_test.go
@@ -0,0 +1,24 @@
+package collector_test
+
+import (
+	"testing"
+
+	"github.com/leorolland/sortir.in/pkg/application"
+	"github.com/leorolland/sortir.in/pkg/infrastructure/collector"
+)
+
+func TestRandomCollectorSuccess(t *testing.T) {
+	collector := collector.NewRandomCollector()
+	events, err := collector.Collect(application.CollectLocation{
+		City: "Paris",
+		Long: 2.3522,
+		Lat:  48.8566,
+		Radius: 10000,
+	})
+	if err != nil {
+		t.Fatalf("failed to collect events: %v", err)
+	}
+	if len(events) != 3 {
+		t.Fatalf("expected 3 events, got %d", len(events))
+	}
+}
